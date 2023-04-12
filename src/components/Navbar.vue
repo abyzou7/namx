4487e283ce3836e446b51fc4c0a93a4c827548a9
Kassim Mohamed <kassim91000@gmail.com>
2023-04-12 12:43:27 +0200

    add of <head> for SEO

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,13 @@
 <template>
+  <head>
+    <meta charset="utf-8">
+    <title>NamX, l'unique SUV à hydrogène</title>
+    <meta name="description" content="Découvrez NamX, un site vitrine présentant notre entreprise innovante. Chez NamX, nous croyons en la technologie verte pour un avenir meilleur. Venez découvrir notre mission et nos solutions durables pour une planète plus verte.">
+    <meta name="keywords" content="NamX, voiture, hydrogène, caspule, hybride, recharge, automobile">
+    <meta name="author" content="NamX ESGI">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  
     <nav class="navbar navbar-expand-lg navbar-transparent bg-transparent">
       <div class="container">
         <a class="navbar-brand" href="#">
